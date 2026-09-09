e4668e8bf6e48b626d8a19bb5651a011bd4e147f
rev <rev@example.com>
2026-09-08 17:35:37 -0700

    Fixed list flattening access logic from _grid[(y*_gridRows) + x] to _grid[(y*_gridCols) + x] as we need to skip cols when we go to the next row, finished Calcualte Neighbors/update for StepHeat and added _prevGrid and _nextGrid for stable updates

--- src/GridSimulation.cpp
+++ src/GridSimulation.cpp
@@ -2,16 +2,49 @@
 #include <string>
 #include "GridSimulation.h"
 
-GridSimulation::GridSimulation(std::vector<float>& grid, int rows, int cols) : _grid(grid), _gridRows(rows), _gridCols(cols) {
+GridSimulation::GridSimulation(std::vector<float>& grid, std::vector<float>& prevGrid, std::vector<float>& nextGrid,
+                            int rows, int cols) : _grid(grid), _prevGrid(prevGrid), _nextGrid(nextGrid), 
+                            _gridRows(rows), _gridCols(cols) {
 
 }
 
 
-float GridSimulation::CalculateNeighbors(int x, int y, std::string variant = "heat") {
-    // u_xx = (u(x+dx) - 2u(x) - u(x-dx)) / (dx)^2
-    if (variant == "heat") {
-        float x_component = _grid
+
+
+float GridSimulation::CalculateNeighbors(int x, int y, float dx, float dy) {
+    float u_xx = 0.0f;
+    float u_yy = 0.0f;
+    // u_xx = (u(x+dx) - 2u(x) + u(x-dx)) / (dx)^2
+    
+    // Forward Difference if Left/Top Boundary
+    // Backward Difference if Right/Bottom Boundary
+    // Central Difference if Middle
+    if (x == 0) {
+        u_xx = (_grid[(y * _gridCols) + x] - (2 * _grid[(y * _gridCols) + (x + 1)]) + _grid[(y * _gridCols) + (x + 2)]); 
     }
+    else if (x == _gridCols - 1) {
+        u_xx = (_grid[(y * _gridCols) + (x - 2)] - (2 * _grid[(y * _gridCols) + (x - 1)]) + _grid[(y * _gridCols) + x]);
+    }
+    else {
+        u_xx = (_grid[(y * _gridCols) + (x - 1)] - (2 * _grid[(y * _gridCols) + x]) + _grid[(y * _gridCols) + (x + 1)]);
+    }
+    
+
+    if (y == 0) {
+        u_yy = (_grid[(y * _gridCols) + x] - (2 * _grid[((y + 1) * _gridCols) + x]) + _grid[((y + 2) * _gridCols) + x]);
+    }
+    else if (y == _gridRows - 1) {
+        u_yy = (_grid[((y - 2) * _gridCols) + x] - (2 * _grid[((y - 1) * _gridCols) + x]) + _grid[(y * _gridCols) + x]);
+    }
+    else {
+        u_yy = (_grid[((y - 1) * _gridCols) + x] - (2 * _grid[(y * _gridCols) + x]) + _grid[((y + 1) * _gridCols) + x]);
+    }
+
+    u_xx /= (dx * dx);
+    u_yy /= (dy * dy);
+    
+    return (u_xx + u_yy);
+    
 }
 
 
@@ -21,11 +54,13 @@ void GridSimulation::StepHeat(float alpha, float dx, float dy, float dt) {
     // Plug this into our heat equation
     // (u(t + dt) - u(t)) / dt = alpha * (u_xx + u_yy)
     // u(t + dt) = u(t) + (dt * alpha * (u_xx + u_yy))
-    
     // Grid is flattened for memory efficiency if grid is M x N, _grid[y][x] = _grid[(y * M) + x]
+
     for (int y = 0; y < _gridRows; ++y) {
         for (int x = 0; x < _gridCols; ++x) {
-            _grid[(y * gridRows) + x] 
+            _nextGrid[(y * _gridCols) + x] = _grid[(y * _gridCols) + x] + (dt * alpha * CalculateNeighbors(x, y, dx, dy));
         }
     }
+    _prevGrid = _grid;
+    _grid = _nextGrid;
 }
--- src/GridSimulation.h
+++ src/GridSimulation.h
@@ -8,6 +8,8 @@ public:
 
 private:
     std::vector<float>& _grid;
+    std::vector<float>& _prevGrid;
+    std::vector<float>& _nextGrid;
     int _gridRows, _gridCols;
     void StepHeat(float alpha, float dx, float dt);
 
