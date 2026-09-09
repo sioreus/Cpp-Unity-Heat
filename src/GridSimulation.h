34d53547eb88bff8fb9ea825c7253196fae9138d
rev <rev@example.com>
2026-09-09 15:28:20 -0700

    Finished UnityBridge.cpp updated GridSimulation.h to match GridSimulation.cpp, included resize to account for adjusting grid size

--- src/GridSimulation.h
+++ src/GridSimulation.h
@@ -4,13 +4,14 @@
 
 class GridSimulation {
 public:
-    GridSimulation(std::vector<float>& grid, int rows, int cols);
+    GridSimulation(std::vector<float>& grid, std::vector<float>&prevGrid, std::vector<float>&nextGrid, int rows, int cols);
 
 private:
     std::vector<float>& _grid;
     std::vector<float>& _prevGrid;
     std::vector<float>& _nextGrid;
     int _gridRows, _gridCols;
-    void StepHeat(float alpha, float dx, float dt);
+    float CalculateNeighbors(int x, int y, int dx, int dy);
+    void StepHeat(float alpha, float dx, float dy, float dt);
 
 };
---
+++ src/GridUnityBridge.cpp
@@ -0,0 +1,53 @@
+#include "GridSimulation.h"
+#include <vector>
+#include <string>
+
+#if defined(_MSC_VER)
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
+
+// Global DLL Variables
+static std::vector<float> g_Grid;
+static std::vector<float> g_prevGrid;
+static std::vector<float> g_nextGrid;
+static GridSimulation* g_GridSimulation = nullptr;
+static int g_Width = 0;
+static int g_Height = 0;
+
+extern "C" {
+    EXPORT void InitGrid(int width, int height) {
+        if (g_GridSimulation) {
+            delete g_GridSimulation;
+        }
+        g_Grid.resize(width * height, 0.0f);
+        g_prevGrid.resize(width * height, 0.0f);
+        g_nextGrid.resize(width * height, 0.0f);
+        g_Width = width;
+        g_Height = height;
+
+        g_GridSimulation = new GridSimulation(g_Grid, g_prevGrid, g_nextGrid, height, width);
+    }
+
+    EXPORT int StepHeatNative(float alpha, float dx, float dy, float dt, float* outGrid, int max) {
+        
+        if (!g_GridSimulation) {
+            return 0;
+        }
+
+        g_GridSimulation->StepHeat(alpha, dx, dy, dt);
+
+        int full_updated = 0;
+
+        for (int y = 0; y < g_Height; ++y) {
+            for (int x = 0; x < g_Width; ++x) {
+                outGrid[(y * g_Width) + x] = g_Grid[(y * g_Width) + x];
+                full_updated += 1;
+            }
+        }
+
+        return full_updated;
+
+    }
+}
